1c340204bb4a16ba1f197552a4848e6f9adfa42d
Julciik <jul.kolinko@gmail.com>
2019-12-24 13:21:18 +0100

    functions, slider, all res

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -37,18 +37,3 @@ window.addEventListener("load", function() {
     ]
   });
 });
-
-let hamburger = document.querySelector(".nav__hamburger"),
-  menu = document.querySelector(".header__nav");
-
-hamburger.addEventListener("click", function() {
-  menu.classList.toggle("header__nav--open");
-});
-
-window.addEventListener("scroll", function() {
-  if (window.pageYOffset > 0) {
-    menu.classList.add("header__nav--fixed");
-  } else {
-    menu.classList.remove("header__nav--fixed");
-  }
-});
